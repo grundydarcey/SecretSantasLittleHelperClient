be7420ed69ffcdc2ff1190c59c0382ccf2c4d945
Beyonce Knowles <you@example.com>
2021-02-05 22:01:45 -0500

    styling

--- src/Rules/rules.js
+++ src/Rules/rules.js
@@ -8,8 +8,9 @@ export default class Rules extends React.Component {
       <div className="rules">
         <h1>Let's Play!</h1>
         <div className="explanation">
-          <p>Time to learn about your Secret Santa group. On the next screen, you will see a hypothetical Secret Santa group of a fake 'nuclear family'. Don't worry, if your group looks a little different, we still have you covered. You can edit each of these members if they aren't quite right. Or if they're not right at all, you can just delete them all too! If you're left with no one on the board, you'll can still add in new members. So throw in your neighbor, your best friend, Jake from State Farm, or anyone else who wants in.</p><br />
-          <p>Don't forget these member blocks also have a spot to indicate how much the Secret Santa present will cost! This is a widely debated part of Secret Santa, so we're leaving it up to you as well.</p><br />
+          <p>On the next page you have the chance to tell us about your Secret Santa group!</p>    
+          <p>You'll see a nuclear family that no one really has on the next page. But don't worry, you can edit or delete each of these members. If you're left with no one on the board, you'll can still add in new members. So throw in your neighbor, your best friend, Jake from State Farm, or anyone else who wants in.</p><br />
+          <p>Don't forget to write in a dollar amount limit so everyone knows how much to spend on their person!</p><br />
           <Link to='/members'>Create Your Secret Santa Group</Link>
         </div>
       </div>
