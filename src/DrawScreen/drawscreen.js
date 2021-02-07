2e2437a7f993e8a1927474417f98169854a826e0
Beyonce Knowles <you@example.com>
2021-02-07 16:38:22 -0500

    styling

--- src/DrawScreen/drawscreen.js
+++ src/DrawScreen/drawscreen.js
@@ -53,8 +53,9 @@ export default class DrawScreen extends React.Component {
         <h1>Begin Drawing</h1>
         <div className="drawbody">
           <p>Here comes the fun part.</p>
-          <p>Now we can start drawing names. Below there is a selector that will show names of everyone in your group. We recommend for this app, you gather everyone around your device. You will select your name from the app, hit 'Start Drawing!' and the next screen will show the name of the person you wil buy a gift for!</p>
-          <p>On the next page, you will see the name of the person YOU will buy a gift for and a button to click. Once you've noted the name, press the button and pass the device to the next person. That button will prevent your secret from getting out!</p><br />
+          <p>Now we can start drawing names. Select your name below, hit 'See Your Secret Match!' and the next screen will show the name of the person you wil buy a gift for!</p>
+          <p>On the next page, you will see the name of the person YOU will buy a gift for and a button to click.</p>
+          <p>Once you've noted the name, press the button and pass the device to the next person. That button will prevent your secret from getting out!</p><br />
           <form>
             <label htmlFor="yourname" id="yourname">Select your name: </label>
             <select 
