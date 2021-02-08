59b7b79836e9b133b6aed25cbb9865909e2acfa8
Beyonce Knowles <you@example.com>
2021-02-07 21:54:29 -0500

    typo

--- src/Rules/rules.js
+++ src/Rules/rules.js
@@ -10,7 +10,7 @@ export default class Rules extends React.Component {
         <h1>Let's Play!</h1>
         <div className="explanation">
           <p>On the next page you have the chance to tell us about your Secret Santa group!</p>    
-          <p>You'll see a nuclear family that no one really has on the next page. But don't worry, you can edit or delete each of these members. If you're left with no one on the board, you'll can still add in new members. So throw in your neighbor, your best friend, Jake from State Farm, or anyone else who wants in.</p><br />
+          <p>You'll see a nuclear family that no one really has on the next page. But don't worry, you can edit or delete each of these members. If you're left with no one on the board, you can still add in new members. So throw in your neighbor, your best friend, Jake from State Farm, or anyone else who wants in.</p><br />
           <p>Don't forget to write in a dollar amount limit so everyone knows how much to spend on their person!</p><br />
           <Link to='/members'>Create Your Secret Santa Group</Link>
         </div>
