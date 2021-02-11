51ea30b074314fa28f9b4ab7e11dfc2b7231c6d7
Beyonce Knowles <you@example.com>
2021-02-11 17:12:39 -0500

    santabutton

--- src/LandingPage/landing-page.js
+++ src/LandingPage/landing-page.js
@@ -9,7 +9,7 @@ export default class LandingPage extends React.Component {
       <div className="landingPage">
         <div className="textbox">
           <p>Thank you for downloading our app so we can celebrate the holiday season with you! It's time to throw out the Santa hat with the doodled name scraps inside, gather around the phone and let's bring Secret Santa into the future together.</p><br /><br />
-          <Link to='/rules'>Let's Get Started</Link>
+          <Link to='/rules'>Let's Get Started</Link><br /><br />
         </div>
     </div>
     )
