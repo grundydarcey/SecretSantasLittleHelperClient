6ecd9271594f86bcd188e5a42b1fe60d84a7af7a
Beyonce Knowles <you@example.com>
2021-02-12 17:44:11 -0500

    editing rules css

--- src/LandingPage/landing-page.js
+++ src/LandingPage/landing-page.js
@@ -8,7 +8,9 @@ export default class LandingPage extends React.Component {
     return (
       <div className="landingPage">
         <div className="textbox">
+          <div className="text">
           <p>Thank you for downloading our app so we can celebrate the holiday season with you! It's time to throw out the Santa hat with the doodled name scraps inside, gather around the phone and let's bring Secret Santa into the future together.</p><br /><br />
+          </div>
           <Link to='/rules'>Let's Get Started</Link><br /><br />
         </div>
     </div>
