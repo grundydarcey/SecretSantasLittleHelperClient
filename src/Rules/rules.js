6bfc3b2adad7c6b791eed815e9b112c43d838b67
Beyonce Knowles <you@example.com>
2021-02-13 18:11:00 -0500

    button fix rules

--- src/Rules/rules.js
+++ src/Rules/rules.js
@@ -12,7 +12,7 @@ export default class Rules extends React.Component {
           <p>On the next page you have the chance to tell us about your Secret Santa group!</p>    
           <p>You'll see a nuclear family that no one really has on the next page. But don't worry, you can edit or delete each of these members. If you're left with no one on the board, you can still add in new members. So throw in your neighbor, your best friend, Jake from State Farm, or anyone else who wants in.</p><br />
           <p>Don't forget to write in a dollar amount limit so everyone knows how much to spend on their person!</p><br />
-          <Link to='/members'>Create Your Secret Santa Group</Link>
+          <Link to='/members'>Create Your Secret Santa Group</Link><br /><br />
         </div>
       </div>
     )
